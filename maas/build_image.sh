ff6db8e20bcf015ddd1f672febf610b6dbb27e01
Muqi Li <muqi1029@gmail.com>
2026-03-05 14:58:40 +0800

    Update maas
    Signed-off-by: Muqi Li <muqi1029@gmail.com>

--- maas/build_image.sh
+++ maas/build_image.sh
@@ -4,7 +4,7 @@ set -exuo pipefail
 
 IMAGE="harbor.weizhipin.com/arsenal-ai/maas-tensorrt-llm-shortcut"
 # TAG="v1.2.0rc2-protocol-log"
-TAG="1.3.0rc6-Ada"
+TAG="v1.3.0rc7"
 
-docker build --network=host -t $IMAGE:$TAG --platform=linux/amd64 -f Dockerfile .
+docker build --network=host -t $IMAGE:$TAG --platform=linux/amd64 -f maas/Dockerfile .
 docker push $IMAGE:$TAG
