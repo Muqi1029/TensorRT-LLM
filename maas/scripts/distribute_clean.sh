5589a983727099c104e8b01e373b387511ff4532
Muqi Li <muqi1029@gmail.com>
2026-04-02 07:40:37 +0000

    [Release] maas-tensorrt-llm-shortcut:v1.3.0rc9
    Signed-off-by: Muqi Li <muqi1029@gmail.com>

--- maas/scripts/distribute_clean.sh
+++ maas/scripts/distribute_clean.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 python scripts/build_wheel.py \
 	--cuda_architectures "89-real;80-real;120-real;90-real" \
 	--install \
-	--no-venv --extra-cmake-vars ENABLE_UCX=OFF
+	--no-venv --extra-cmake-vars ENABLE_UCX=OFF --clean
